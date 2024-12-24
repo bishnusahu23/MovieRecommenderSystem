e2f6be76b8085806ea75a6299857b3128c98c2a8
Bishnu K Sahu <sahu.bishnukumari@gmail.com>
2024-12-24 20:52:58 +0530

    Add files via upload

---
+++ MovieRecommenderApp.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pickle
+import numpy as np
+import requests
+
+try:
+    with open(r"C:\Users\sahub\Desktop\Folders\Movie_recommendor_system\Formatted_movies.pkl",'rb') as file:
+        movies=pickle.load(file)
+    with open(r"C:\Users\sahub\Desktop\Folders\Movie_recommendor_system\cosine_similarity.pkl",'rb') as file:
+        similarity=pickle.load(file)
+except Exception as e:
+    st.write(f"Error loading data: {e}")
+
+
+st.title("Movie Recommeder System")
+choice=st.selectbox("Pick one:",options= list(movies['title']))
+num=int(st.number_input("Select number of recommendations",placeholder="Enter a digit", min_value=1, step=1))
+
+def fetch_posters(id):
+    url='https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(id)
+    data= requests.get(url)
+    data=data.json()
+    poster_path=data['poster_path']
+    full_path="https://image.tmdb.org/t/p/w500/" +poster_path
+    return full_path
+
+
+def recommend(name, count):
+    try:
+        idx=movies[movies['title']==name].index[0]
+        indexes=list(np.argsort(similarity[idx])[-count-1:-1])
+        movies_df=movies.loc[indexes][['title','id']]
+        recommended_movies=[]
+        posters=[]
+        for index,row in movies_df.iterrows():
+            movie_id=row['id']
+            posters.append(fetch_posters(movie_id))
+            recommended_movies.append(row['title'])
+        return recommended_movies,posters
+        
+    except:
+        st.write('No recommendations available')
+        
+if st.button('Recommend'):
+    recommended_movies, posters = recommend(choice, num)
+    if recommended_movies:
+        for name, poster in zip(recommended_movies, posters):
+            with st.container():
+                col1, col2 = st.columns([1,4])  
+                with col1:
+                    st.image(poster, width=100)   
+                with col2:
+                    st.write(name)                
+                    st.markdown('---') 
+    else:
+        st.write("No recommendations available.")
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
