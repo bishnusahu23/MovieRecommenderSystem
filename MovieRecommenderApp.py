f3d4cc85c0dfbc9767f3e721859dd09b3dcd92a0
Bishnu K Sahu <sahu.bishnukumari@gmail.com>
2025-01-07 16:55:28 +0530

    Update MovieRecommenderApp.py

--- MovieRecommenderApp.py
+++ MovieRecommenderApp.py
@@ -3,69 +3,68 @@ import pickle
 import numpy as np
 import requests
 import gdown
+import gzip
 
+# Load Movies and Cosine Similarity from Google Drive
 try:
-    with open(r"Formatted_movies.pkl",'rb') as file:
-        movies=pickle.load(file)
-    with open(r"cosine_similarity.pkl",'rb') as file:
-        similarity=pickle.load(file)
+    with open("Formatted_movies.pkl", 'rb') as file:
+        movies = pickle.load(file)
+
+    # Google Drive file ID extracted
+    file_url = "https://drive.google.com/uc?export=download&id=1reqRIPB3oatkqCqzk5XpK13m88mq9jBE"
+    
+    # Download the file using gdown
+    gdown.download(file_url, 'cosine_similarity.pkl', quiet=False)
+
+    # Load the cosine similarity matrix
+    with open('cosine_similarity.pkl', 'rb') as file:
+        cosine_sim = pickle.load(file)
+
 except Exception as e:
     st.write(f"Error loading data: {e}")
 
+# Streamlit Title
+st.title("Movie Recommender System")
 
-st.title("Movie Recommeder System")
-choice=st.selectbox("Pick one:",options= list(movies['title']))
-num=int(st.number_input("Select number of recommendations",placeholder="Enter a digit", min_value=1, step=1))
+# User Input Section
+choice = st.selectbox("Pick a movie:", options=list(movies['title']))
+num = int(st.number_input("Number of recommendations:", min_value=1, step=1))
 
-def fetch_posters(id):
-    url='https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(id)
-    data= requests.get(url)
-    data=data.json()
-    poster_path=data['poster_path']
-    full_path="https://image.tmdb.org/t/p/w500/" +poster_path
-    return full_path
+# Fetch Movie Poster Function
+def fetch_posters(movie_id):
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+    if poster_path:
+        return f"https://image.tmdb.org/t/p/w500/{poster_path}"
+    return None
 
-
+# Movie Recommendation Function
 def recommend(name, count):
     try:
-        idx=movies[movies['title']==name].index[0]
-        indexes=list(np.argsort(similarity[idx])[-count-1:-1])
-        movies_df=movies.loc[indexes][['title','id']]
-        recommended_movies=[]
-        posters=[]
-        for index,row in movies_df.iterrows():
-            movie_id=row['id']
-            posters.append(fetch_posters(movie_id))
-            recommended_movies.append(row['title'])
-        return recommended_movies,posters
+        idx = movies[movies['title'] == name].index[0]
+        indexes = list(np.argsort(cosine_sim[idx])[-count-1:-1])
+        movies_df = movies.loc[indexes][['title', 'id']]
         
-    except:
-        st.write('No recommendations available')
-        
+        recommended_movies = []
+        posters = []
+        for _, row in movies_df.iterrows():
+            poster = fetch_posters(row['id'])
+            if poster:
+                posters.append(poster)
+                recommended_movies.append(row['title'])
+        return recommended_movies, posters
+    except Exception as e:
+        st.write(f"Error generating recommendations: {e}")
+        return [], []
+
 if st.button('Recommend'):
     recommended_movies, posters = recommend(choice, num)
     if recommended_movies:
-        for name, poster in zip(recommended_movies, posters):
-            with st.container():
-                col1, col2 = st.columns([1,4])  
-                with col1:
-                    st.image(poster, width=100)   
-                with col2:
-                    st.write(name)                
-                    st.markdown('---') 
+        cols = st.columns(5)  # Adjust the number of columns based on your layout preference
+        for index, (name, poster) in enumerate(zip(recommended_movies, posters)):
+            with cols[index % 5]:  # This will wrap the items after every 5 movies
+                st.write(name)
+                st.image(poster, width=150)
     else:
         st.write("No recommendations available.")
-    
-
-
-    
-
-
-
-
-
-
-
-
-
-
